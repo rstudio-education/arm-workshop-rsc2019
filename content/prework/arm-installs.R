a4445d25de4d3f5e7ad3b06b2b74c9fe1c4db3b0
Alison Hill <apreshill@gmail.com>
2019-01-04 09:11:09 -0800

    update purled install script

--- content/prework/arm-installs.R
+++ content/prework/arm-installs.R
@@ -1,7 +1,14 @@
-## ----install_pkgs, purl = TRUE-------------------------------------------
-arm_pkgs <- c("xaringan", "flexdashboard",
-              "blogdown", "bookdown", "tidyverse")
-install.packages(arm_pkgs, dependencies = TRUE)
+## ----cran_pkgs, purl = TRUE, eval = TRUE---------------------------------
+arm_from_cran <- c("flexdashboard", "bookdown", "tidyverse", "remotes")
+
+## ----install_cran, purl = TRUE-------------------------------------------
+install.packages(arm_from_cran, dependencies = TRUE)
+
+## ----gh_pkgs, purl = TRUE, eval = TRUE-----------------------------------
+arm_from_gh <- c('yihui/xaringan', 'rstudio/blogdown')
+
+## ----install_gh, purl = TRUE---------------------------------------------
+remotes::install_github(arm_from_gh, dependencies = TRUE)
 
 ## ----install_check_function, include = FALSE, purl = TRUE----------------
 is_installed <- function(pkg_name){
@@ -10,8 +17,8 @@ is_installed <- function(pkg_name){
 
 ## ----blogdown_hugo, purl = TRUE------------------------------------------
 blogdown::hugo_version() # to check your version
-blogdown::update_hugo() # to force an update
+# blogdown::update_hugo() # to force an update
 
 ## ----check_all_installs, eval = TRUE, purl = TRUE------------------------
-is_installed(arm_pkgs)
+is_installed(c(arm_from_cran, 'xaringan', 'blogdown'))
 
